b0792896af51b9020a3e144eedbff831abd76c17
Jorge Martinez <jorgemg0327@gmail.com>
2017-06-07 10:01:48 -0500

    Indent

--- users.sh
+++ users.sh
@@ -3,34 +3,34 @@
 #############################################################################
 
 sudo -u $USER PGPASSWORD=$DB_PW psql -U $DB_USER -h $DB_HOST $OLD_DB -c \
-  "COPY(SELECT id,
-    password,
-    last_login,
-    is_superuser,
-    username,
-    first_name,
-    last_name,
-    email,
-    is_staff,
-    is_active,
-    date_joined
-  FROM auth_user) to stdout with csv" | \
+    "COPY(SELECT id,
+        password,
+        last_login,
+        is_superuser,
+        username,
+        first_name,
+        last_name,
+        email,
+        is_staff,
+        is_active,
+        date_joined
+    FROM auth_user) to stdout with csv" | \
 sudo -u $USER psql $NEW_DB -c \
-  "COPY people_profile (id,
-    password,
-    last_login,
-    is_superuser,
-    username,
-    first_name,
-    last_name,
-    email, 
-    is_staff,
-    is_active,
-    date_joined)
-  FROM STDIN CSV"
+    "COPY people_profile (id,
+        password,
+        last_login,
+        is_superuser,
+        username,
+        first_name,
+        last_name,
+        email, 
+        is_staff,
+        is_active,
+        date_joined)
+    FROM STDIN CSV"
 
 echo "\nReset user sequence"; do_dash
 sudo -u $USER psql $NEW_DB -c \
-  "SELECT setval(pg_get_serial_sequence('people_profile', 'id'),
-    coalesce(max(id),0) + 1, false)
-  FROM people_profile;"
+    "SELECT setval(pg_get_serial_sequence('people_profile', 'id'),
+        coalesce(max(id),0) + 1, false)
+    FROM people_profile;"
