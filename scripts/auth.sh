cf3f7445499d21508d51e800d10c4bb24198e2c2
Jorge Martinez <jorgemg0327@gmail.com>
2017-07-12 16:03:21 -0500

    Do not truncate profile_groups table

--- scripts/auth.sh
+++ scripts/auth.sh
@@ -6,7 +6,7 @@ echo "\nAuth_group table migration"; do_dash
 sudo -u $USER PGPASSWORD=$DB_PW psql -U $DB_USER -h $DB_HOST $NEW_DB -c "UPDATE auth_group SET name = 'beta-users' WHERE id=1; INSERT INTO auth_group (name) VALUES ('dataverse');"
 
 echo "\n people_profile_groups table migration"; do_dash
-sudo -u $USER PGPASSWORD=$DB_PW psql -U $DB_USER -h $DB_HOST $NEW_DB -c "TRUNCATE TABLE people_profile_groups;"
+# sudo -u $USER PGPASSWORD=$DB_PW psql -U $DB_USER -h $DB_HOST $NEW_DB -c "TRUNCATE TABLE people_profile_groups;"
 
 sudo -u $USER PGPASSWORD=$DB_PW \
 psql -v ON_ERROR_STOP=1 -U $DB_USER -h $DB_HOST $OLD_DB -c \
