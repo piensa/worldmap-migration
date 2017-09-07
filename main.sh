3188340098fba2f0d5ffb7b7afaa82858ecc57d1
Jorge Martinez <jorgemg0327@gmail.com>
2017-09-07 11:17:09 -0500

    Comment fixture loading

--- main.sh
+++ main.sh
@@ -77,7 +77,7 @@ source $ENV_PATH/bin/activate
 python $GEONODE_PATH/manage.py makemigrations
 python $GEONODE_PATH/manage.py makemigrations datatables certification
 python $GEONODE_PATH/manage.py migrate
-python $GEONODE_PATH/manage.py loaddata $GEONODE_PATH/fixtures/default_oauth_apps.json
+# python $GEONODE_PATH/manage.py loaddata $GEONODE_PATH/fixtures/default_oauth_apps.json
 else
 do_hr
 echo "Generating tables from django server"
@@ -88,7 +88,7 @@ source /home/ubuntu/wm.sh
 python manage.py makemigrations
 python manage.py makemigrations datatables certification
 python manage.py migrate
-python manage.py loaddata fixtures/default_oauth_apps.json
+# python manage.py loaddata fixtures/default_oauth_apps.json
 EOF
 fi
 
